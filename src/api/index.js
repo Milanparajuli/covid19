97ed74d2b7bccb449f2c4754333cd8b667b2f73b
Sajan Kc <sajan1996kc@gmail.com>
2020-11-27 12:50:48 +0545

    chart added

--- src/api/index.js
+++ src/api/index.js
@@ -19,3 +19,19 @@ export const fetchData = async () => {
     console.log(error);
   }
 };
+
+export const fetchDailyData = async () => {
+  try {
+    const { data } = await axios.get(`${baseURL}/daily`);
+
+    const modifiedData = data.map((dailyData) => ({
+      confirmed: dailyData.confirmed.total,
+      deaths: dailyData.deaths.total,
+      date: dailyData.reportDate,
+    }));
+
+    return modifiedData;
+  } catch (error) {
+    console.log(error);
+  }
+};
